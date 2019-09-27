75843da4b8c989247b84a4c217d9f0ecd7b0a461
Divyangi <divyangisingh@outlook.com>
2019-09-28 04:15:30 +0530

    Spline Area Graph without gradient using Canvas.js

---
+++ Spine Area Graph Code/index.js
@@ -0,0 +1,57 @@
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,  
+	title:{
+		text: "Spending Trend"
+	},
+	axisY: {
+		title: "Revenue in USD",
+		valueFormatString: "#0,,.",
+		suffix: "mn",
+		prefix: "$"
+	},
+	data: [{
+		type: "splineArea",
+		 
+        color2: "#33b6a7",
+        
+      
+		markerSize: 5,
+		xValueFormatString: "YYYY",
+		yValueFormatString: "$#,##0.##",
+		dataPoints: [
+			{ x: new Date(2000, 0), y: 3289000 },
+			{ x: new Date(2001, 0), y: 3830000 },
+			{ x: new Date(2002, 0), y: 2009000 },
+			{ x: new Date(2003, 0), y: 2840000 },
+			{ x: new Date(2004, 0), y: 2396000 },
+			{ x: new Date(2005, 0), y: 1613000 },
+			{ x: new Date(2006, 0), y: 2821000 },
+			{ x: new Date(2007, 0), y: 2000000 },
+			{ x: new Date(2008, 0), y: 1397000 },
+			{ x: new Date(2009, 0), y: 2506000 },
+			{ x: new Date(2010, 0), y: 2798000 },
+			{ x: new Date(2011, 0), y: 3386000 },
+			{ x: new Date(2012, 0), y: 6704000},
+			{ x: new Date(2013, 0), y: 6026000 },
+			{ x: new Date(2014, 0), y: 2394000 },
+			{ x: new Date(2015, 0), y: 1872000 },
+			{ x: new Date(2016, 0), y: 2140000 }
+		]
+	}]
+	});
+chart.render();
+
+var options = ['img', 'name', 'date','amount'];
+
+var list = [{img:"img/img1",name: "Netflix", date:"78", amount:"8$"},
+{img:"img/img1",name: "Netflix", date:"78", amount:"8$"},
+{img:"img/img1",name: "Netflix", date:"78", amount:"8$"},
+{img:"img/img1",name: "Netflix", date:"78", amount:"8$"}];
+
+var parselist = JSON.parse(list);
+this.document.getElementById('#img').innerHTML = parsedlist.name;
+
+
+}
